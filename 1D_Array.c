2468e069c666604bb3932b664d3642b9509b163c
Jaswant Singh <54163759+Jaswantsinghh@users.noreply.github.com>
2020-04-05 18:39:41 +0530

    added a function "delete" to delete an element.

--- 1D_Array.c
+++ 1D_Array.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int add(int arr[], int position, int value);
 int search(int *arr,int key);
+int deleteElement(int arr[], int n, int x);
 int main()
 {
     int arr[5] = {2,3,1,6,0}; /*intialising an array*/
@@ -44,4 +45,25 @@ int i;
 return 0;
 }
 
+
+int deleteElement(int arr[], int n, int x) /*Function to delete an element from an array*/
+{ 
+   // Search x in array 
+   int i; 
+   for (i=0; i<n; i++) 
+      if (arr[i] == x) 
+         break; 
+  
+   // If x found in array 
+   if (i < n) 
+   { 
+     // reduce size of array and move all 
+     // elements on space ahead 
+     n = n - 1; 
+     for (int j=i; j<n; j++) 
+        arr[j] = arr[j+1]; 
+   } 
+  
+   return n; 
+} 
 
