2591bc2739c185cff348772554088335490ea73d
Jaswant Singh <54163759+Jaswantsinghh@users.noreply.github.com>
2020-04-05 17:54:06 +0530

    Update 1D_Array.c
    added a function "add" to add a new value in an existing array.

--- 1D_Array.c
+++ 1D_Array.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
-
+int add(int arr[], int position, int value);
 int main()
 {
-    int arr[10]; /*intialising an array*/
-
+    int arr[5] = {2,3,1,6,0}; /*intialising an array*/
+    /*Call any function here */
     return 0;
 }
+
+
+int add(int arr[],int position,int value) /*Function to add a new value at a position in an existing array*/
+{
+  int new_array[6],i,j,k;
+   for(i=0;i<position;i++)
+   {
+       new_array[i]=arr[i];
+   }
+   new_array[position]=value;
+   for(j=position+1;j<7;j++)
+   {
+       new_array[j]=arr[j-1];
+   }
+   for(k=0;k<6;k++)
+   {
+       printf("%d\n",new_array[k]);
+   }
+  return new_array;
+}
+
+
