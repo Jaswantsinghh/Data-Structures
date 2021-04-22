db4a0ab4095db696602291ae610f204a1ba6af26
Jaswant Singh <54163759+Jaswantsinghh@users.noreply.github.com>
2021-04-22 11:16:49 +0530

    Update Simple_sort.cpp

--- Array/Sorting/Simple_sort.cpp
+++ Array/Sorting/Simple_sort.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int main()
 {
+    //initialised an array of size 10
     int arr[10]={2,4,1,0,6,9,8,3,5,7};
+    //initialisation of temp variable to store a variable temporarialy
     int temp;
     for(int i=0;i<10;++i)
     {
@@ -11,6 +13,7 @@ int main()
         {
             if(arr[i]<arr[j])
             {
+                //interchanging value of arr[i] and arr[j
                 temp=arr[i];
                 arr[i]=arr[j];
                 arr[j]=temp;
@@ -20,6 +23,7 @@ int main()
     }
     for(int i=0;i<10;++i)
     {
+        //displaying all the elements of array after sorting
         cout << arr[i];
     }
     return 0;
