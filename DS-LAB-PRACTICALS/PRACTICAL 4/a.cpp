d2e83eb2c5bd448afd7c6fccc5bb4472a2bcdb7f
Jaswant Singh <54163759+Jaswantsinghh@users.noreply.github.com>
2021-04-28 09:29:23 +0530

    inline documentation done

--- DS-LAB-PRACTICALS/PRACTICAL 4/a.cpp
+++ DS-LAB-PRACTICALS/PRACTICAL 4/a.cpp
@@ -2,6 +2,8 @@
 // STL library
 #include<bits/stdc++.h>
 using namespace std;
+
+//function to compute calculation between operands and operator
 float compute(char symbol, float op1, float op2)
 
 {
@@ -28,6 +30,7 @@ float compute(char symbol, float op1, float op2)
 
 }
 
+//Driver function
 int main()
 
 {
@@ -35,15 +38,15 @@ int main()
 float s[20], res, op1, op2;
 
 int top, i;
-
+// initialised a char array to make stack
 char postfix[20], symbol;
 
 cout << "\nEnter the postfix expression:\n";
-
+// input postfix expression
 cin >> postfix;
-
+// top of stack is initialised to -1 which means stack is empty
 top=-1;
-
+// traversing and checking comdition for calculations
 for (i=0; i<strlen(postfix) ;i++)
 
 {
@@ -61,7 +64,7 @@ else
  op2 = s[top--];
 
  op1 = s[top--];
-
+// calling compute function and storing return value in res variable
  res = compute(symbol, op1, op2);
 
  s[++top] = res;
@@ -71,7 +74,7 @@ else
 }
 
 res = s[top--];
-
+// output of answer
 cout << "\nThe result is : "<<res<<endl;
 
 } 
